cc822dca71fbdee0740c466685e256ed55967606
Ferran Franco i Moral <45915950+Xarcu@users.noreply.github.com>
2024-10-16 09:56:05 +0200

    Add files via upload

---
+++ init_env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+conda init bash
+if ! conda list --name tdcs_predict; then
+    echo 'Creating Environment'
+    conda config --add channels conda-forge
+    conda env create --file=./environment.yml
+fi
+conda activate tdcs_predict
+conda env update -n tdcs_predict --file environment.yml --prune
+
+python -m pip install --upgrade pip
+
+export PYTHONPATH=${PWD}
+
+
