59e7d6e62857eafba7f2a18a93236e920cbfd571
Ferran Franco i Moral <ferranfrancomoral@gmail.com>
2024-10-16 12:45:19 +0200

    [NEW]

--- definitions.py
+++ definitions.py
@@ -2,7 +2,7 @@ import os
 
 CHANNELS = ['FC3', 'FCz', 'FC4', 'C3', 'Cz', 'C4', 'CP3', 'CPz','CP4']
 
-CHAN_LOC = r".\Documents\Standard-10-20-Cap10.txt"
+CHAN_LOC = r".\Documents\Standard-10-20-Cap10.locs"
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
